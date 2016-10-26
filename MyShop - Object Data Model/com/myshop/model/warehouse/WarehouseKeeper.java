e82da0da3bd08573f1393355e8d61dd413f73157
Guillermo Facundo Colunga <colunga91@gmail.com>
2016-10-26 13:20:54 +0200

    Added Warehouse Keeper and Working plan to the model
    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

---
+++ MyShop - Object Data Model/com/myshop/model/warehouse/WarehouseKeeper.java
@@ -0,0 +1,43 @@
+package com.myshop.model.warehouse;
+
+/**
+ * Warehouse keeper ODM
+ * 
+ * @author Guillermo Facundo Colunga
+ */
+public class WarehouseKeeper {
+	
+	private int warehouseKeeperID;
+	private String name, surname;
+	
+	/**
+	 * @return the warehouse keeper id.
+	 */
+	public int getWarehouseKeeperID() { return this.warehouseKeeperID; }
+	
+	/**
+	 * @param warehouseKeeperID the warehouse keeper id.
+	 */
+	public void setWarehouseKeeperID(int warehouseKeeperID) { this.warehouseKeeperID = warehouseKeeperID; }
+	
+	/**
+	 * @return the name of the warehouse keeper.
+	 */
+	public String getName() { return this.name; }
+	
+	/**
+	 * @param name the name of the warehouse keeper.
+	 */
+	public void setName(String name) { this.name = name; }
+	
+	/**
+	 * @return the surname of the warehouse keeper.
+	 */
+	public String getSurname() { return this.surname; }
+	
+	/**
+	 * @param surname the surname of the warehouse keeper.
+	 */
+	public void setSurname(String surname) { this.surname = surname; }
+
+}
---
+++ MyShop - Object Data Model/com/myshop/model/workingPlan/WorkingPlan.java
@@ -0,0 +1,28 @@
+package com.myshop.model.workingPlan;
+
+public class WorkingPlan {
+
+	private int wpID, wkID;
+
+	/**
+	 * @return the wpID
+	 */
+	public int getWpID() { return wpID; }
+
+	/**
+	 * @param wpID the wpID to set
+	 */
+	public void setWpID(int wpID) { this.wpID = wpID; }
+
+	/**
+	 * @return the wkID
+	 */
+	public int getWkID() { return wkID; }
+
+	/**
+	 * @param wkID the wkID to set
+	 */
+	public void setWkID(int wkID) { this.wkID = wkID; }
+	
+	
+}
